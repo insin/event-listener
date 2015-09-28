a245ce7aa2333d529eb9314ac9da8bcdc485de07
ruffle1986 <ftamas.mail@gmail.com>
2015-09-28 13:57:49 +0200

    support adding listener to multiple events

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 'use strict';
 
 function listen(target, eventType, callback) {
+  if (typeof eventType != 'string') {
+    throw new TypeError('eventType must be a string')
+  }
+  var eventTypes = eventType.split(' ').filter(Boolean)
   if (target.addEventListener) {
-    target.addEventListener(eventType, callback, false)
+    eventTypes.forEach(function (eventType) {
+      target.addEventListener(eventType, callback, false)
+    })
     return {
       remove: function() {
-        target.removeEventListener(eventType, callback, false)
+        eventTypes.forEach(function (eventType) {
+          target.removeEventListener(eventType, callback, false)
+        })
       }
     }
   }
   else if (target.attachEvent) {
-    target.attachEvent('on' + eventType, callback)
+    eventTypes.forEach(function (eventType) {
+      target.attachEvent('on' + eventType, callback)
+    })
     return {
       remove: function() {
-        target.detachEvent('on' + eventType, callback)
+        eventTypes.forEach(function (eventType) {
+          target.detachEvent('on' + eventType, callback)
+        })
       }
     }
   }
